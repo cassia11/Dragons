a4abaf9398397e26e33541a0e2e9e63ed88533c9
cassia11 <cassiagomes0102@gmail.com>
2020-02-15 18:24:44 -0300

    created navbar

--- src/pages/root/App.js
+++ src/pages/root/App.js
@@ -1,25 +1,39 @@
 import './App.css';
 import React from 'react';
-import Home from '../dragons/Home';
+import HomePage from '../dragons/HomePage';
 import ListPage from '../dragons/ListPage';
-import { Link, Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <Link to='/'>Home</Link>
-        <Link to='/dragon'>Dragon</Link>
-
-        <Switch>
-          <Route exact path='/' component={ Home } />
-          <Route exact path='/dragon' component={ ListPage } />
-        </Switch>
-
-      </header>
-    </div>
+    <Navbar className="App-navbar" expand="lg">
+      <Navbar.Brand href="#home">
+        <img
+          src="dragon.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="Imagem de um dragão"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link bg="light" href="/">Página inicial</Nav.Link>
+          <Nav.Link bg="light" href="/dragons">Lista de dragões</Nav.Link>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/dragons' component={ListPage} />
+          </Switch>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
+
 
 export default App;

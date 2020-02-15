9a7ccae9fe21d8a4107c8b1f949dd0c3d1daee86
cassia11 <cassiagomes0102@gmail.com>
2020-02-15 20:12:42 -0300

    position image

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Image from 'react-bootstrap/Image'
+import './HomePage.css';
+
 
 class Home extends React.Component {
 
   render() {
     return (
-      <Image src="dragonHome.jpg" fluid />
+      <div className="HomePage-div">
+        <Image className="HomePage-image" src="dragonHome.jpg" fluid />
+      </div>
     );
   }
 }

7e38e91581d4f90b542e1a20b260d33e4e2773c9
cassia11 <cassiagomes0102@gmail.com>
2020-02-16 09:57:31 -0300

    adjuste cards

--- src/pages/dragons/ListPage.js
+++ src/pages/dragons/ListPage.js
@@ -1,19 +1,11 @@
 import React from 'react'
-import { Card } from 'react-bootstrap';
+import CardDragons from '../../components/CardDragons'
 
 class ListPage extends React.Component {
 
   render() {
     return (
-      <div>
-        <Card style={{ width: '18rem', margin: '30px' }}>
-          <Card.Body>
-            <Card.Title bg="light">Nome do dragão</Card.Title>
-            <Card.Link bg="light" href="#">Remover dragão</Card.Link>
-            <Card.Link bg="light" href="#">Editar dragão</Card.Link>
-          </Card.Body>
-        </Card>
-      </div>
+    <CardDragons></CardDragons>
     );
   }
 }

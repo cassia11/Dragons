25d5f9a00c475dcc436ccce44f2e5a379e2b0597
cassia11 <cassiagomes0102@gmail.com>
2020-02-16 23:30:17 -0300

    adjuste edit dragon

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -5,13 +5,14 @@ import { Route } from 'react-router-dom'
 import DetailsPage from '../pages/dragons/DetailsPage';
 
 const handleDelete = (idDragon) => {
-    console.log(idDragon);
-    axios.delete(`/${idDragon}`)
-        .then(res => {
-            console.log(res);
-            console.log(res.data);
-            window.location.reload();
-        })
+    try {
+        axios.delete(`/${idDragon}`)
+            .then(res => {
+                window.location.reload();
+            })
+    } catch (err) {
+        console.log(`😱 Axios request failed: ${err}`);
+    }
 }
 
 const ListCard = ({ dragon }, key) => (
@@ -20,13 +21,14 @@ const ListCard = ({ dragon }, key) => (
             <Card.Body>
                 <Card.Title bg="light">{dragon.name}</Card.Title>
                 <Button variant="danger" onClick={() => handleDelete(dragon.id)} >Remover</Button>
-                <Card.Link path={`/dragons/${dragon.id}`} >
-                    <Button variant="primary" >Editar</Button>
-                </Card.Link>
+                <Button variant="primary" type="button" onClick={() => null }>
+                    {dragon.id}
+                </Button>
+                <Card.Link to={`/${dragon.id}`} >Editar</Card.Link>
             </Card.Body>
             <Card.Footer className="text-muted">{dragon.createdAt}</Card.Footer>
         </Card>
-        <Route path='/dragons/:id' component={ DetailsPage } ></Route>
+        <Route path='/dragons/:id' component={DetailsPage} ></Route>
     </div>
 );
 

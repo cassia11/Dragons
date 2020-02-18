9872e72ea731888effc2243c1c2ce18bec45e62b
cassia11 <cassiagomes0102@gmail.com>
2020-02-18 08:53:13 -0300

    refactor details card

--- src/components/DetailsCard.js
+++ src/components/DetailsCard.js
@@ -1,32 +1,59 @@
 import React from 'react'
-import { Card, Form, Col } from 'react-bootstrap'
+import axios from '../configs/api'
+import { Form, Col, Button } from 'react-bootstrap'
+
+const form = {
+    name: '',
+    type: ''
+};
+
+const handleSubmit = (event, idDragon) => {
+    console.log(event)
+    try {
+        axios.put(`/${idDragon}`, { form })
+            .then(res => {
+                console.log(res)
+            })
+    } catch (err) {
+        console.log(`😱 Axios request failed: ${err}`);
+    }
+}
+
+const handleChange = (event) => {
+    const name = event.target.name
+    const value = event.target.value
+
+    if (name === 'name') form.name = value
+    else form.type = value
+    console.log(form)
+}
+
+const handleField = ({ dragon }) => {
+    const name = dragon.name
+}
 
 const DetailsCard = ({ dragon }, key) => (
     <div>
         <h1>Altere os dados do Dragão</h1>
-        <Form>
+        <Form onSubmit={(event) => handleSubmit(event, dragon.id)}>
             <Form.Row>
                 <Col>
                     <Form.Label>Nome:</Form.Label>
-                    <Form.Control />
+                    <Form.Control defaultValue={dragon.name} name="name" onChange={(event) => handleChange(event)} />
                 </Col>
                 <Col>
                     <Form.Label>Tipo:</Form.Label>
-                    <Form.Control />
+                    <Form.Control defaultValue={dragon.type} name="type" onChange={(event) => handleChange(event)} />
                 </Col>
                 <Col>
                     <Form.Label>Data de Criação:</Form.Label>
-                    <Form.Control />
+                    <Form.Control value={dragon.createdAt} disabled />
                 </Col>
             </Form.Row>
+            <Form.Row>
+                <Col><Button type="submit">Atualizar</Button></Col>
+            </Form.Row>
         </Form>
-        <Card style={{ width: '50%', margin: '30px' }}>
-            <Card.Body>
-                <Card.Title>nome: {dragon.name}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Tipo: {dragon.type}</Card.Subtitle>
-                <Card.Text>Criado em: {dragon.createdAt}</Card.Text>
-            </Card.Body>
-        </Card>
     </div>
 )
 

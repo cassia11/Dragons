727b9f7b3a0ab6669838ef5b822d53e9f09c2eb1
cassia11 <cassiagomes0102@gmail.com>
2020-02-18 17:52:30 -0300

    adjustment layout of register and login

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ const Login = () => (
             <Form.Label>Senha:</Form.Label>
             <Form.Control type="password" placeholder="Enter password" />
         </Form.Group>
-        <Button variant="outline-success">Salvar</Button>
+        <Button style={{  backgroundColor: 'pink', borderColor: 'pink' }}>Salvar</Button>
     </Form>
 );
 export default Login

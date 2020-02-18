67f82f7254ac8669845bf650bfce2a9306aa6e58
cassia11 <cassiagomes0102@gmail.com>
2020-02-18 18:00:33 -0300

    remove placeholder

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,11 +6,11 @@ const Login = () => (
     <Form style={{ paddingTop: '16px', padding: '20px' }}>
         <Form.Group controlId="formBasicUsername">
             <Form.Label>Nome de usuário:</Form.Label>
-            <Form.Control type="username" placeholder="Enter username" />
+            <Form.Control type="username" />
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
             <Form.Label>Senha:</Form.Label>
-            <Form.Control type="password" placeholder="Enter password" />
+            <Form.Control type="password" />
         </Form.Group>
         <Button style={{  backgroundColor: 'pink', borderColor: 'pink' }}>Entrar</Button>
     </Form>

880e016b6dd79d2fcbcb5032f671d4d464937bf4
cassia11 <cassiagomes0102@gmail.com>
2020-02-18 22:53:26 -0300

    dotenv

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+const API = process.env.API_URL
 const API_URL = 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon'
 
 export default axios.create({
--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import App from './pages/root/App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
+require('dotenv').config({
+    path: process.env.NODE_ENV === "development" ? ".env.testing" : ".env"
+  })
+console.log(process.env.APP_NAME)
+console.log(process.env.ENVIROMENT)
+
 ReactDOM.render(
     <BrowserRouter>
         <App/>
